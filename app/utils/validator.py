b69bd02cf5a772dc3e2dae5905b61e750798fb74
paridhi-ramtani-iitmbs <25f1002763@ds.study.iitm.ac.in>
2025-11-13 01:55:32 +0530

    Update validator.py

--- app/utils/validator.py
+++ app/utils/validator.py
@@ -1,1 +1,12 @@
+"""
+Request validation
+"""
+from app.config import settings
+import hmac
 
+def validate_request(secret: str) -> bool:
+    """Validate secret key"""
+    try:
+        return hmac.compare_digest(secret, settings.SECRET_KEY)
+    except:
+        return False
